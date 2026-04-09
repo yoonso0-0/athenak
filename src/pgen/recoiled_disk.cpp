0fb8e8dc714f61298f22705fb416eaa5d1e2899e
Yoonsoo Kim <ykim7@caltech.edu>
2026-04-09 20:39:38 +0000

    Add user boundary condition and history

--- src/pgen/recoiled_disk.cpp
+++ src/pgen/recoiled_disk.cpp
@@ -47,6 +47,10 @@ recoiled_disk_pgen recoiled_disk;
 
 } // namespace
 
+// Prototypes for user-defined BCs and history functions
+void HydroNoInflow(Mesh *pm);
+void RecoiledDiskHistory(HistoryData *pdata, Mesh *pm);
+
 //----------------------------------------------------------------------------------------
 //! \fn void ProblemGenerator::UserProblem()
 //
@@ -67,6 +71,12 @@ void ProblemGenerator::UserProblem(ParameterInput *pin, const bool restart) {
         << std::endl;
     exit(EXIT_FAILURE);
   }
+
+  // @YK : setting up boundary condition
+  user_bcs_func = HydroNoInflow;
+
+  //
+  user_hist_func = RecoiledDiskHistory;
 
   // return if restart
   if (restart)
@@ -194,6 +204,333 @@ void ProblemGenerator::UserProblem(ParameterInput *pin, const bool restart) {
             0.5 * (SQR(u0_(m, IM1, k, j, i)) + SQR(u0_(m, IM2, k, j, i)) +
                    SQR(u0_(m, IM3, k, j, i)));
       });
+
+  return;
+}
+
+//
+// @YK: Hydro no-inflow boundary condition
+//
+//  (Apr 2026) Only supports for hydro case. Look gr_torus.cpp for possible
+//  extension to MHD or radiation
+//
+void HydroNoInflow(Mesh *pm) {
+  auto &indcs = pm->mb_indcs;
+  int &ng = indcs.ng;
+  int n1 = indcs.nx1 + 2 * ng;
+  int n2 = (indcs.nx2 > 1) ? (indcs.nx2 + 2 * ng) : 1;
+  int n3 = (indcs.nx3 > 1) ? (indcs.nx3 + 2 * ng) : 1;
+  int &is = indcs.is;
+  int &ie = indcs.ie;
+  int &js = indcs.js;
+  int &je = indcs.je;
+  int &ks = indcs.ks;
+  int &ke = indcs.ke;
+  auto &mb_bcs = pm->pmb_pack->pmb->mb_bcs;
+
+  // Select either Hydro or MHD
+  DvceArray5D<Real> u0_, w0_;
+  if (pm->pmb_pack->phydro != nullptr) {
+    u0_ = pm->pmb_pack->phydro->u0;
+    w0_ = pm->pmb_pack->phydro->w0;
+  } else if (pm->pmb_pack->pmhd != nullptr) {
+    u0_ = pm->pmb_pack->pmhd->u0;
+    w0_ = pm->pmb_pack->pmhd->w0;
+
+    std::cout << "### FATAL ERROR in " << __FILE__ << " at line " << __LINE__
+              << std::endl
+              << " MHD should not be enabled for this problem! " << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  int nmb = pm->pmb_pack->nmb_thispack;
+  int nvar = u0_.extent_int(1);
+
+  // Determine if radiation is enabled
+  const bool is_radiation_enabled = (pm->pmb_pack->prad != nullptr);
+  if (is_radiation_enabled) {
+    std::cout << "### FATAL ERROR in " << __FILE__ << " at line " << __LINE__
+              << std::endl
+              << " Radiation should not be enabled for this problem! "
+              << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  // ConsToPrim over all X1 ghost zones *and* at the innermost/outermost
+  // X1-active zones of Meshblocks, even if Meshblock face is not at the edge of
+  // computational domain
+  if (pm->pmb_pack->phydro != nullptr) {
+    pm->pmb_pack->phydro->peos->ConsToPrim(u0_, w0_, false, is - ng, is, 0,
+                                           (n2 - 1), 0, (n3 - 1));
+    pm->pmb_pack->phydro->peos->ConsToPrim(u0_, w0_, false, ie, ie + ng, 0,
+                                           (n2 - 1), 0, (n3 - 1));
+  } else if (pm->pmb_pack->pmhd != nullptr) {
+    std::cout << "### FATAL ERROR in " << __FILE__ << " at line " << __LINE__
+              << std::endl
+              << " MHD should not be enabled for this problem! " << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  // Set X1-BCs on w0 if Meshblock face is at the edge of computational domain
+  par_for(
+      "noinflow_hydro_x1", DevExeSpace(), 0, (nmb - 1), 0, (nvar - 1), 0,
+      (n3 - 1), 0, (n2 - 1), KOKKOS_LAMBDA(int m, int n, int k, int j) {
+        if (mb_bcs.d_view(m, BoundaryFace::inner_x1) == BoundaryFlag::user) {
+          for (int i = 0; i < ng; ++i) {
+            if (n == (IVX)) {
+              w0_(m, n, k, j, is - i - 1) = fmin(0.0, w0_(m, n, k, j, is));
+            } else {
+              w0_(m, n, k, j, is - i - 1) = w0_(m, n, k, j, is);
+            }
+          }
+        }
+        if (mb_bcs.d_view(m, BoundaryFace::outer_x1) == BoundaryFlag::user) {
+          for (int i = 0; i < ng; ++i) {
+            if (n == (IVX)) {
+              w0_(m, n, k, j, ie + i + 1) = fmax(0.0, w0_(m, n, k, j, ie));
+            } else {
+              w0_(m, n, k, j, ie + i + 1) = w0_(m, n, k, j, ie);
+            }
+          }
+        }
+      });
+
+  // PrimToCons on X1 ghost zones
+  if (pm->pmb_pack->phydro != nullptr) {
+    pm->pmb_pack->phydro->peos->PrimToCons(w0_, u0_, is - ng, is - 1, 0,
+                                           (n2 - 1), 0, (n3 - 1));
+    pm->pmb_pack->phydro->peos->PrimToCons(w0_, u0_, ie + 1, ie + ng, 0,
+                                           (n2 - 1), 0, (n3 - 1));
+  } else if (pm->pmb_pack->pmhd != nullptr) {
+    std::cout << "### FATAL ERROR in " << __FILE__ << " at line " << __LINE__
+              << std::endl
+              << " MHD should not be enabled for this problem! " << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  // X2-Boundary
+  // Set X2-BCs on b0 if Meshblock face is at the edge of computational domain
+  if (pm->pmb_pack->pmhd != nullptr) {
+    auto &b0 = pm->pmb_pack->pmhd->b0;
+    par_for(
+        "noinflow_field_x2", DevExeSpace(), 0, (nmb - 1), 0, (n3 - 1), 0,
+        (n1 - 1), KOKKOS_LAMBDA(int m, int k, int i) {
+          if (mb_bcs.d_view(m, BoundaryFace::inner_x2) == BoundaryFlag::user) {
+            for (int j = 0; j < ng; ++j) {
+              b0.x1f(m, k, js - j - 1, i) = b0.x1f(m, k, js, i);
+              if (i == n1 - 1) {
+                b0.x1f(m, k, js - j - 1, i + 1) = b0.x1f(m, k, js, i + 1);
+              }
+              b0.x2f(m, k, js - j - 1, i) = b0.x2f(m, k, js, i);
+              b0.x3f(m, k, js - j - 1, i) = b0.x3f(m, k, js, i);
+              if (k == n3 - 1) {
+                b0.x3f(m, k + 1, js - j - 1, i) = b0.x3f(m, k + 1, js, i);
+              }
+            }
+          }
+          if (mb_bcs.d_view(m, BoundaryFace::outer_x2) == BoundaryFlag::user) {
+            for (int j = 0; j < ng; ++j) {
+              b0.x1f(m, k, je + j + 1, i) = b0.x1f(m, k, je, i);
+              if (i == n1 - 1) {
+                b0.x1f(m, k, je + j + 1, i + 1) = b0.x1f(m, k, je, i + 1);
+              }
+              b0.x2f(m, k, je + j + 2, i) = b0.x2f(m, k, je + 1, i);
+              b0.x3f(m, k, je + j + 1, i) = b0.x3f(m, k, je, i);
+              if (k == n3 - 1) {
+                b0.x3f(m, k + 1, je + j + 1, i) = b0.x3f(m, k + 1, je, i);
+              }
+            }
+          }
+        });
+  }
+  // ConsToPrim over all X2 ghost zones *and* at the innermost/outermost
+  // X2-active zones of Meshblocks, even if Meshblock face is not at the edge of
+  // computational domain
+  if (pm->pmb_pack->phydro != nullptr) {
+    pm->pmb_pack->phydro->peos->ConsToPrim(u0_, w0_, false, 0, (n1 - 1),
+                                           js - ng, js, 0, (n3 - 1));
+    pm->pmb_pack->phydro->peos->ConsToPrim(u0_, w0_, false, 0, (n1 - 1), je,
+                                           je + ng, 0, (n3 - 1));
+  } else if (pm->pmb_pack->pmhd != nullptr) {
+    std::cout << "### FATAL ERROR in " << __FILE__ << " at line " << __LINE__
+              << std::endl
+              << " MHD should not be enabled for this problem! " << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  // Set X2-BCs on w0 if Meshblock face is at the edge of computational domain
+  par_for(
+      "noinflow_hydro_x2", DevExeSpace(), 0, (nmb - 1), 0, (nvar - 1), 0,
+      (n3 - 1), 0, (n1 - 1), KOKKOS_LAMBDA(int m, int n, int k, int i) {
+        if (mb_bcs.d_view(m, BoundaryFace::inner_x2) == BoundaryFlag::user) {
+          for (int j = 0; j < ng; ++j) {
+            if (n == (IVY)) {
+              w0_(m, n, k, js - j - 1, i) = fmin(0.0, w0_(m, n, k, js, i));
+            } else {
+              w0_(m, n, k, js - j - 1, i) = w0_(m, n, k, js, i);
+            }
+          }
+        }
+        if (mb_bcs.d_view(m, BoundaryFace::outer_x2) == BoundaryFlag::user) {
+          for (int j = 0; j < ng; ++j) {
+            if (n == (IVY)) {
+              w0_(m, n, k, je + j + 1, i) = fmax(0.0, w0_(m, n, k, je, i));
+            } else {
+              w0_(m, n, k, je + j + 1, i) = w0_(m, n, k, je, i);
+            }
+          }
+        }
+      });
+
+  // PrimToCons on X2 ghost zones
+  if (pm->pmb_pack->phydro != nullptr) {
+    pm->pmb_pack->phydro->peos->PrimToCons(w0_, u0_, 0, (n1 - 1), js - ng,
+                                           js - 1, 0, (n3 - 1));
+    pm->pmb_pack->phydro->peos->PrimToCons(w0_, u0_, 0, (n1 - 1), je + 1,
+                                           je + ng, 0, (n3 - 1));
+  } else if (pm->pmb_pack->pmhd != nullptr) {
+    std::cout << "### FATAL ERROR in " << __FILE__ << " at line " << __LINE__
+              << std::endl
+              << " MHD should not be enabled for this problem! " << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  // X3-Boundary
+  // Set X3-BCs on b0 if Meshblock face is at the edge of computational domain
+
+  // ConsToPrim over all X3 ghost zones *and* at the innermost/outermost
+  // X3-active zones of Meshblocks, even if Meshblock face is not at the edge of
+  // computational domain
+  if (pm->pmb_pack->phydro != nullptr) {
+    pm->pmb_pack->phydro->peos->ConsToPrim(u0_, w0_, false, 0, (n1 - 1), 0,
+                                           (n2 - 1), ks - ng, ks);
+    pm->pmb_pack->phydro->peos->ConsToPrim(u0_, w0_, false, 0, (n1 - 1), 0,
+                                           (n2 - 1), ke, ke + ng);
+  } else if (pm->pmb_pack->pmhd != nullptr) {
+    std::cout << "### FATAL ERROR in " << __FILE__ << " at line " << __LINE__
+              << std::endl
+              << " MHD should not be enabled for this problem! " << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  // Set X3-BCs on w0 if Meshblock face is at the edge of computational domain
+  par_for(
+      "noinflow_hydro_x3", DevExeSpace(), 0, (nmb - 1), 0, (nvar - 1), 0,
+      (n2 - 1), 0, (n1 - 1), KOKKOS_LAMBDA(int m, int n, int j, int i) {
+        if (mb_bcs.d_view(m, BoundaryFace::inner_x3) == BoundaryFlag::user) {
+          for (int k = 0; k < ng; ++k) {
+            if (n == (IVZ)) {
+              w0_(m, n, ks - k - 1, j, i) = fmin(0.0, w0_(m, n, ks, j, i));
+            } else {
+              w0_(m, n, ks - k - 1, j, i) = w0_(m, n, ks, j, i);
+            }
+          }
+        }
+        if (mb_bcs.d_view(m, BoundaryFace::outer_x3) == BoundaryFlag::user) {
+          for (int k = 0; k < ng; ++k) {
+            if (n == (IVZ)) {
+              w0_(m, n, ke + k + 1, j, i) = fmax(0.0, w0_(m, n, ke, j, i));
+            } else {
+              w0_(m, n, ke + k + 1, j, i) = w0_(m, n, ke, j, i);
+            }
+          }
+        }
+      });
+
+  // PrimToCons on X3 ghost zones
+  if (pm->pmb_pack->phydro != nullptr) {
+    pm->pmb_pack->phydro->peos->PrimToCons(w0_, u0_, 0, (n1 - 1), 0, (n2 - 1),
+                                           ks - ng, ks - 1);
+    pm->pmb_pack->phydro->peos->PrimToCons(w0_, u0_, 0, (n1 - 1), 0, (n2 - 1),
+                                           ke + 1, ke + ng);
+  } else if (pm->pmb_pack->pmhd != nullptr) {
+    std::cout << "### FATAL ERROR in " << __FILE__ << " at line " << __LINE__
+              << std::endl
+              << " MHD should not be enabled for this problem! " << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  return;
+}
+
+//
+// @YK: History variables
+//
+void RecoiledDiskHistory(HistoryData *pdata, Mesh *pm) {
+  // MeshBlockPack *pmbp = pm->pmb_pack;
+
+  pdata->nhist = 3;
+  pdata->label[0] = "Lx";
+  pdata->label[1] = "Ly";
+  pdata->label[2] = "Lz";
+
+  // loop over all MeshBlocks in this pack
+  auto &indcs = pm->pmb_pack->pmesh->mb_indcs;
+  int is = indcs.is;
+  int nx1 = indcs.nx1;
+  int js = indcs.js;
+  int nx2 = indcs.nx2;
+  int ks = indcs.ks;
+  int nx3 = indcs.nx3;
+  const int nmkji = (pm->pmb_pack->nmb_thispack) * nx3 * nx2 * nx1;
+  const int nkji = nx3 * nx2 * nx1;
+  const int nji = nx2 * nx1;
+
+  // capture class variables for kernel
+  auto &u0_ = pm->pmb_pack->phydro->u0;
+  auto &w0_ = pm->pmb_pack->phydro->w0;
+  auto &size = pm->pmb_pack->pmb->mb_size;
+  int &nhist_ = pdata->nhist;
+
+  array_sum::GlobalSum sum_this_mb;
+
+  Kokkos::parallel_reduce(
+      "gravitational_drag", Kokkos::RangePolicy<>(DevExeSpace(), 0, nmkji),
+      KOKKOS_LAMBDA(const int &idx, array_sum::GlobalSum &mb_sum) {
+        // compute n,k,j,i indices of thread
+        int m = (idx) / nkji;
+        int k = (idx - m * nkji) / nji;
+        int j = (idx - m * nkji - k * nji) / nx1;
+        int i = (idx - m * nkji - k * nji - j * nx1) + is;
+        k += ks;
+        j += js;
+
+        Real vol = size.d_view(m).dx1 * size.d_view(m).dx2 * size.d_view(m).dx3;
+
+        Real &x1min = size.d_view(m).x1min;
+        Real &x1max = size.d_view(m).x1max;
+        const Real x1v = CellCenterX(i - is, indcs.nx1, x1min, x1max);
+
+        Real &x2min = size.d_view(m).x2min;
+        Real &x2max = size.d_view(m).x2max;
+        const Real x2v = CellCenterX(j - js, indcs.nx2, x2min, x2max);
+
+        Real &x3min = size.d_view(m).x3min;
+        Real &x3max = size.d_view(m).x3max;
+        const Real x3v = CellCenterX(k - ks, indcs.nx3, x3min, x3max);
+
+        // angular momentum
+        array_sum::GlobalSum hvars;
+
+        const Real px = u0_(m, IM1, k, j, i);
+        const Real py = u0_(m, IM2, k, j, i);
+        const Real pz = u0_(m, IM3, k, j, i);
+
+        // l_x
+        hvars.the_array[0] = vol * (x2v * pz - x3v * py);
+        hvars.the_array[1] = vol * (x3v * px - x1v * pz);
+        hvars.the_array[2] = vol * (x1v * py - x2v * px);
+
+        // fill rest of the_array with zeros, if nhist < NHISTORY_VARIABLES
+        for (int n = nhist_; n < NHISTORY_VARIABLES; ++n) {
+          hvars.the_array[n] = 0.0;
+        }
+
+        // sum into parallel reduce
+        mb_sum += hvars;
+      },
+      Kokkos::Sum<array_sum::GlobalSum>(sum_this_mb));
+
+  // store data into hdata array
+  for (int n = 0; n < pdata->nhist; ++n) {
+    pdata->hdata[n] = sum_this_mb.the_array[n];
+  }
 
   return;
 }
